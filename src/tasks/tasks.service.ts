aa845d5695b23f6de00c7c31cdf69d2bf68836f4
MyFirstRepo <adityasaah932@gmail.com>
2026-03-06 18:46:17 +0530

    Feat : Added the tasks controller and a get service to get all the tasks

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Tasks } from './tasks-interface';
+
+@Injectable()
+export class TasksService {
+  tasks: Tasks[] = [
+    {
+      id: 1,
+      title: 'Learn Nest',
+      description: 'Follow all the steps to learn it',
+      isCompleted: false,
+    },
+  ];
+
+  getAll() {
+    return this.tasks;
+  }
+}
